14ffa2bb402e11f8c2d001049cf050329d8c696c
quanght22 <quanght22@gmail.com>
2018-11-12 00:35:37 +0700

    aaaa

--- WebApp/src/components/layout/content.js
+++ WebApp/src/components/layout/content.js
@@ -5,7 +5,7 @@ export default class Content extends Component {
   render() {
     return (
       <div className="content">
-        {this.props.children}
+         {this.props.children}
       </div>
     );
   }
